301cfed93ac6838a0a071b8d000ab363882811a9
abhirupGhosh1992 <joshmcgrath123@gmail.com>
2021-06-03 23:18:23 +0530

    added dynamic loading from config

--- lib/widgets/ui/TutorialCardsView.dart
+++ lib/widgets/ui/TutorialCardsView.dart
@@ -1,30 +1,49 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'GlowingCardsLayout.dart';
 
-class TutorialCardsView extends StatelessWidget {
-  final size = 10000;
-
+class TutorialCardsView extends StatefulWidget {
   TutorialCardsView({final key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() {
+    return _TutorialCardsViewState();
+  }
+}
+
+class _TutorialCardsViewState extends State<TutorialCardsView> {
+  var _itemCount = 0;
+  List<dynamic> itemList = List<dynamic>.empty();
+  void _readConfig() async {
+    String config = await rootBundle.loadString('extras/config.json');
+    Map<String, dynamic> config_json = jsonDecode(config);
+    setState(() {
+      _itemCount = itemList.length;
+      itemList = config_json["title_cards_tutorials"];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _readConfig();
+
     return Expanded(
         child: ListView.builder(
-            itemCount: 10000,
+            itemCount: _itemCount,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
               return GlowingCardsLayout(
-                imageUrl: "images/svg/test.svg",
-                headerText: "Mobile\nDevelopment",
-                subText: "72.7% of all devices run Android in one form or the other. This makes android one of the most sought after mobile platforms for business\n\n" +
-                    "With technologies like ract native and flutter we can generate multiple apps for different platforms using a single code base.\n\n" +
-                    "So our mobile phones are soon becoming the centre of our lives and having the ability to control the centre is really fruitful.",
+                imageUrl: itemList[index]['icon_url'],
+                headerText: itemList[index]['header'],
+                subText: itemList[index]['sub_text'],
                 logo: "",
                 margin: EdgeInsets.only(
                     top: 100,
                     left: index == 0 ? 50 : 10,
-                    right: index == size - 1 ? 50 : 10,
+                    right: index == _itemCount - 1 ? 50 : 10,
                     bottom: 100),
               );
             }));
